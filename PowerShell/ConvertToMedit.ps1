b642db7d62d4a8ba4bb2e51b90293af85671d8e9
Davessonn <babaidavid96@gmail.com>
2023-05-24 16:40:13 +0200

    final

--- PowerShell/ConvertToMedit.ps1
+++ PowerShell/ConvertToMedit.ps1
@@ -15,39 +15,76 @@ if (-not $OutputFile) {
 # Olvassuk be a bemeneti fájlt
 $vrmlContent = Get-Content -Path $InputFile -Raw
 
+# Ellenőrizzük az input fejlécét, hogy megfelel-e
+$firstLine = ($vrmlContent -split "`n")[0]
+$firstLine = $firstLine -split '\s+'
+if ($firstLine[0] -ne "#VRML" -or $firstLine[1] -notmatch '^V[0-9]\.[0-9]' -or $firstLine[2] -ne "utf8") {
+    Write-Host "Incorrect header!"
+    exit 1
+}
+
 # Definiáljuk a MEDIT fájl tartalmát
 $meditContent = @"
 # MEDIT
 MeshVersionFormatted 1
 Dimension 3
 Vertices
+
 "@
 
 # Szövegfeldolgozás és adatok kinyerése a VRML tartalomból
 $pointIndex = $vrmlContent.IndexOf("point [") + 7
 $coordIndexIndex = $vrmlContent.IndexOf("coordIndex [") + 12
 
-$pointsText = $vrmlContent.Substring($pointIndex, $coordIndexIndex - $pointIndex).Trim()
+$pointsText = $vrmlContent.Substring($pointIndex, $coordIndexIndex - $pointIndex)
 $coordIndexText = $vrmlContent.Substring($coordIndexIndex).Trim()
 
 # Koordináták feldolgozása és hozzáadása a MEDIT tartalomhoz
 $points = $pointsText -split '\r?\n' | Where-Object { $_ -match '\d+\.\d+\s\d+\.\d+\s\d+\.\d+' }
 
 $points | ForEach-Object {
-    $point = $_ -split '\s+' | Select-Object -Skip 1
-    $meditContent += "{0} {1} {2} {3}`n" -f ++$node_idx, $point[0], $point[1], $point[2]
+    $point =$_ -split '\s+' | Where-Object {$_ -ne ""}
+    Write-Host "Point: " $point
+    $point | ForEach-Object {
+        $pointIndexes = $_ -split '\s'
+        Write-Host "Pointindexes: "$pointIndexes
+       
+    }
+    
+    if ($pointIndexes.Length -lt 3) {
+        $meditContent += "Not enough point"
+    } elseif ($pointIndexes.Length -gt 3) {
+        $meditContent += "Too much point"
+    } else {
+        $meditContent += "{0} {1} {2}`n" -f $point[0], $point[1], $point[2]
+    }
+        
+    
 }
+Write-Host $pointIndexes.Length
 
 # Háromszögek feldolgozása és hozzáadása a MEDIT tartalomhoz
-$triangles = $coordIndexText -split '\r?\n' | Where-Object { $_ -match '\d+\s\d+\s\d+\s-1' }
-
+$meditContent += "Triangles`n"
+$triangles = $coordIndexText -split '\r?\n' | Where-Object { $_ -match '\d+(?:,\s\d+)*,\s-1' }
 $triangles | ForEach-Object {
-    $indices = $_ -split '\s+' | Select-Object -SkipLast 1
-    $meditContent += "{0} {1} {2} {3}`n" -f ++$elem_idx, $indices[0], $indices[1], $indices[2]
+    $trianglesIndexes = $_.Trim()
+    $trianglesIndexes | ForEach-Object {
+        $coordinates = $_ -split ', '
+        if ($coordinates.Length -lt 4) {
+            $meditContent += "Not enough point for a triangle`n"
+            return
+        } elseif ($coordinates.Length -gt 4) {
+            $meditContent += "Too much point for a triangle`n"
+            return
+        } else {
+            $meditContent += "{0} {1} {2}`n" -f $coordinates[0].Trim(), $coordinates[1].Trim(), $coordinates[2].Trim(), $coordinates[3].Trim()
+        }
+        
+    }
 }
 
 # MEDIT tartalom kimenetének írása a fájlba
 $meditContent += "End"
 $meditContent | Set-Content -Path $OutputFile
 
-Write-Host "A VRML fájl sikeresen átalakítva MEDIT fájlra: $OutputFile"
+Write-Host "The VRML file has been successfully converted to a MEDIT file: $OutputFile"
